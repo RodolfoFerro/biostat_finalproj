615fb725b9d0c4a4a9a42413b4de0ccf13399664
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2016-12-06 15:59:39 -0600

    Upload files

---
+++ credentials.py
@@ -0,0 +1,12 @@
+# Credentials for your Twitter account
+
+# 1. Creae a new app on https://apps.twitter.com/
+# 2. Go to Keys and Access Tokens and generate the Access Token
+# 3. Copy and paste Keys and Access Tokens
+
+CONSUMER_KEY = "USQGWafl3PA03nHCIr6L8yeW0"
+CONSUMER_SECRET = "HBeBgoqBVLfvoniCWOum6s8yyFngPV5TZRqEqQMPbZubWJFKj6"
+
+# Create a new Access Token
+ACCESS_TOKEN = "3007552940-WBX5n332JRXVj70yp3Hem3rYxov5tAo8eNE9noH"
+ACCESS_SECRET = "bkRKGYQSLMWW9Sy3vbBunPT5wK6M5LskbNjrVS2NM3XaN"
